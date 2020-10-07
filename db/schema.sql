c4b19a0c8fd283f0f52ee9432cb3fe0daecc4d71
Carlos <bwaycarl@gmail.com>
2020-10-07 11:29:38 -0400

    Add NFL Teams table.

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,15 @@
-g-- Create Covid Schema
+-- Create Covid Schema
 CREATE DATABASE covid;
 
 USE covid;
 
 -- Create Tables
+
 -- Definition of 'fips': Federal Information Processing Standards - American National Standards Institute codes (ANSI codes)
 -- are standardized numeric or alphabetic codes issued by the American National Standards Institute (ANSI) 
 -- to ensure uniform identification of geographic entities through all federal government agencies.
 -- (https://www.census.gov/quickfacts/fact/note/US/fips)
+
 CREATE TABLE covidCounties 
 (
     id INT NOT NULL AUTO_INCREMENT, 
@@ -19,3 +21,13 @@ CREATE TABLE covidCounties
     deaths INT (11),
     PRIMARY KEY (id)
 );
+
+
+CREATE TABLE NFLTeams (
+id INT (11)NOT NULL AUTO_INCREMENT,
+team VARCHAR (50),
+city VARCHAR (50),
+county VARCHAR (50),
+fips INT (11)
+);
+
