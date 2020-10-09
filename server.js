5285e2203d60bdf9025e93a83f9be7de0698a862
Manuel Pierre <pierrem1.mp@gmail.com>
2020-10-08 20:26:52 -0400

    server.js, .gitignore

--- server.js
+++ server.js
@@ -2,32 +2,58 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var path = require("path");
+var axios = require("axios")
+var config = require("config.js")
 
 // Create an instance of the express app.
 var app = express();
-console.log(path.join(__dirname, '/Assets'));
-app.use(express.static(path.join(__dirname, '/Assets')));
+
+app.use(express.static(path.join(__dirname, "assets")));
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
 // Set Handlebars as the default templating engine.
-app.engine("handlebars", exphbs({
-  defaultLayout: "main"
-}));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+  })
+);
 app.set("view engine", "handlebars");
 
 // Data
 
-
-// Routes
 app.get("/", function (req, res) {
   res.render("index", {});
 });
 
-// app.get("/", function (req, res) {
+// app.get("/teams", function (req, res) {
 //   res.render("index", ...[1]);
 // });
+
+app.get("/teaminfo", function(req, res){
+
+  axios.get(`https://api.sportsdata.io/v3/nfl/scores/json/Schedules/2020?key=${config.apikey1}`)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+    res.json(response)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  
+
+
+
+})
+
+// app.get("/", function (req, res) {
+//       // Grab the selected parameter
+//       var chosen = req.params.selectedTeam;
+//       console.log(chosen);
 
 // app.get("/", function (req, res) {
 //   res.render("all-games", {
