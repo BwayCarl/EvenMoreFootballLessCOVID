c827cc1775d108b716a082012c9eae036e359fa0
Charles Metzger <cmetzjr@yahoo.com>
2020-10-10 10:53:54 -0400

    updated files

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "", //Insert your MySQL password here.
+  password: "password", //Insert your MySQL password here.
   database: "covidCounties"
 });
 
