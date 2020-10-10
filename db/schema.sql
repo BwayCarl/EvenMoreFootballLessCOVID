5ae9d412af0faf2d712473981978e27c675ff9c4
Carlos <bwaycarl@gmail.com>
2020-10-10 13:32:26 -0400

    Add schema and tables.

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,29 @@
--- Create Covid Schema
-CREATE DATABASE covid;
+-- Create EMFLC Schema
 
-USE covid;
+CREATE DATABASE emflcDB;
 
--- Create Tables
+USE emflcDB;
 
--- Definition of 'fips': Federal Information Processing Standards - American National Standards Institute codes (ANSI codes)
--- are standardized numeric or alphabetic codes issued by the American National Standards Institute (ANSI) 
--- to ensure uniform identification of geographic entities through all federal government agencies.
--- (https://www.census.gov/quickfacts/fact/note/US/fips)
-
-CREATE TABLE covidCounties 
-(
-    id INT NOT NULL AUTO_INCREMENT, 
-    DATETIME,
-    county VARCHAR(50),
-    state VARCHAR (20),
-    fips INT(11), 
-    cases INT (11),
-    deaths INT (11),
-    PRIMARY KEY (id)
+CREATE TABLE user (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR (100),
+    pword VARCHAR (128),
+    faveTeam VARCHAR (50)
 );
 
-
-CREATE TABLE NFLTeams (
-id INT (11)NOT NULL AUTO_INCREMENT,
-team VARCHAR (50),
-city VARCHAR (50),
-county VARCHAR (50),
-fips INT (11)
+CREATE TABLE team (
+	id INT NOT NULL,
+    teamName VARCHAR (50),
+    city VARCHAR (50)    
 );
 
+CREATE TABLE game (
+	game_id INT,
+    team_id INT,
+	date DATE
+);
+
+CREATE TABLE news (
+	text VARCHAR(255),
+    team_id INT
+);
